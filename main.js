d6c1f62d5760fa81189ac89ab4c4865324e78259
AntwuanDixon <evanwchipman@gmail.com>
2021-01-25 04:53:00 +0700

    fix event listener bug (hallelujah), add reset button logic and gauge logic

--- main.js
+++ main.js
@@ -5,33 +5,50 @@ const timerSlider = document.querySelector('#timer-slider');
 const timerValue = document.querySelector('#timer-value')
 const timerSecondsElapsed = document.querySelector('#timer-seconds-elapsed');
 const maxTimerSeconds = document.querySelector('#timer-seconds-max');
-timerSecondsElapsed.innerHTML = 0;
+const resetBtn = document.querySelector('#reset-btn');
+var secondsElapsed = 0;
 var kicked = false;
 var timer;
+meter.value = 0;
+timerSecondsElapsed.innerHTML = 0;
+
 
 function stopCount () {
     clearInterval(timer)
 }
 
-timerSlider.addEventListener('input', function () {
+function triggerTimer () {
     maxTimerSeconds.innerHTML = timerSlider.value;
-    currentTime = parseInt(timerSecondsElapsed.innerHTML);
     maxTime = parseInt(timerSlider.value);
-    let i = currentTime;
-
+    var i = secondsElapsed;
+    var meterStep;
     if (kicked === false) {
-        timer = setInterval(countUp, 1000)
+        timer = setInterval(countUp, 1000);
         kicked = true;
-    } else if (currentTime == maxTime) {
-        timer = setInterval(countUp, 1000)
     }
 
     function countUp () {
         if (i >= maxTime) {
-            clearInterval(timer)
+            clearInterval(timer);
+            kicked = false;
+            secondsElapsed = i;
         } else {
             i++;
             timerSecondsElapsed.innerHTML = i;
+            meterStep = 100/maxTime;
+            meter.value = meterStep.toFixed(2) * i;
         }
     }
+}
+
+
+timerSlider.addEventListener('input', function () {
+    triggerTimer ();
+})
+
+resetBtn.addEventListener('click', function () {
+    secondsElapsed = 0;
+    timerSecondsElapsed.innerHTML = secondsElapsed;
+    meter.value = 0;
+    triggerTimer ();
 })
